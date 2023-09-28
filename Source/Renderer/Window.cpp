ea8a740ff5249198ea7cda677b9dd5a306919c27
Poseydon42 <vvmposeydon@gmail.com>
2023-09-28 20:43:09 +0100

    Implemented mouse event handling in Window class

--- Source/Renderer/Window.cpp
+++ Source/Renderer/Window.cpp
@@ -2,6 +2,21 @@
 
 #include "Core/Assert.h"
 
+static MouseButton MouseButtonFromGLFWCode(int Button)
+{
+	switch (Button)
+	{
+	case GLFW_MOUSE_BUTTON_LEFT:
+		return MouseButton::Left;
+	case GLFW_MOUSE_BUTTON_RIGHT:
+		return MouseButton::Right;
+	case GLFW_MOUSE_BUTTON_MIDDLE:
+		return MouseButton::Middle;
+	default:
+		BD_UNREACHABLE();
+	}
+}
+
 static int MouseButtonToGLFWCode(MouseButton Button)
 {
 	switch (Button)
@@ -86,9 +101,9 @@ void Window::PollEvents()
 	double MouseX, MouseY;
 	glfwGetCursorPos(m_Window, &MouseX, &MouseY);
 
-	auto NewMousePosition = glm::vec2(static_cast<float>(MouseX), static_cast<float>(MouseY));
-	m_DeltaMousePosition = NewMousePosition - m_CurrentMousePosition;
-	m_CurrentMousePosition = NewMousePosition;
+	auto NewMousePosition = glm::ivec2(static_cast<int>(std::floor(MouseX)), static_cast<int>(std::floor(MouseY)));
+	m_DeltaCursorPosition = NewMousePosition - m_CurrentCursorPosition;
+	m_CurrentCursorPosition = NewMousePosition;
 }
 
 void Window::SwapBuffers()
@@ -106,14 +121,24 @@ bool Window::ShouldClose() const
 	return glfwWindowShouldClose(m_Window);
 }
 
+void Window::AddMouseButtonCallback(std::function<MouseButtonCallbackType>&& Callback)
+{
+	m_MouseButtonCallbacks.push_back(std::move(Callback));
+}
+
 bool Window::IsMouseButtonPressed(MouseButton Button) const
 {
 	return glfwGetMouseButton(m_Window, MouseButtonToGLFWCode(Button)) == GLFW_PRESS;
 }
 
-glm::vec2 Window::GetMouseDelta() const
+glm::ivec2 Window::GetCursorPosition() const
 {
-	return m_DeltaMousePosition;
+	return m_CurrentCursorPosition;
+}
+
+glm::ivec2 Window::GetCursorDelta() const
+{
+	return m_DeltaCursorPosition;
 }
 
 int32_t Window::GetMouseWheelDelta() const
@@ -130,6 +155,22 @@ Window::Window(GLFWwindow* Window)
 	{
 		static_cast<class Window*>(glfwGetWindowUserPointer(GLFWWindow))->ScrollCallback(YOffset);
 	});
+	glfwSetMouseButtonCallback(m_Window, [](GLFWwindow* GLFWWindow, int Button, int Action, int)
+	{
+		static_cast<class Window*>(glfwGetWindowUserPointer(GLFWWindow))->MouseButtonCallback(Button, Action);
+	});
+}
+
+void Window::MouseButtonCallback(int GLFWButton, int GLFWAction)
+{
+	auto Button = MouseButtonFromGLFWCode(GLFWButton);
+	auto Type = (GLFWAction == GLFW_PRESS ? ButtonEventType::Press : ButtonEventType::Release);
+
+	double CursorX, CursorY;
+	glfwGetCursorPos(m_Window, &CursorX, &CursorY);
+
+	for (const auto& Callback : m_MouseButtonCallbacks)
+		Callback(Button, Type, static_cast<int32_t>(std::floor(CursorX)), static_cast<int32_t>(std::floor(CursorY)));
 }
 
 void Window::ScrollCallback(double YOffset)
--- Source/Renderer/Window.h
+++ Source/Renderer/Window.h
@@ -2,6 +2,7 @@
 
 #define GLFW_INCLUDE_NONE
 
+#include <functional>
 #include <GLFW/glfw3.h>
 #include <memory>
 #include <string_view>
@@ -12,6 +13,12 @@ enum class MouseButton
 	Left,
 	Right,
 	Middle,
+};
+
+enum class ButtonEventType
+{
+	Press,
+	Release,
 };
 
 class Window
@@ -32,18 +39,24 @@ public:
 
 	bool ShouldClose() const;
 
+	using MouseButtonCallbackType = void(MouseButton, ButtonEventType, int32_t, int32_t);
+	void AddMouseButtonCallback(std::function<MouseButtonCallbackType>&& Callback);
+
 	bool IsMouseButtonPressed(MouseButton Button) const;
 
-	glm::vec2 GetMouseDelta() const;
+	glm::ivec2 GetCursorPosition() const;
+	glm::ivec2 GetCursorDelta() const;
 
 	int32_t GetMouseWheelDelta() const;
 
 private:
 	GLFWwindow* m_Window = nullptr;
 
-	glm::vec2 m_CurrentMousePosition = {};
-	glm::vec2 m_DeltaMousePosition = {};
+	glm::ivec2 m_CurrentCursorPosition = {};
+	glm::ivec2 m_DeltaCursorPosition = {};
 	int32_t m_MouseWheelDelta = 0;
+
+	std::vector<std::function<MouseButtonCallbackType>> m_MouseButtonCallbacks;
 
 	static constexpr int s_GLVersionMajor = 4;
 	static constexpr int s_GLVersionMinor = 2;
@@ -52,6 +65,8 @@ private:
 	static int s_InstanceCount;
 
 	explicit Window(GLFWwindow* Window);
+
+	void MouseButtonCallback(int GLFWButton, int GLFWAction);
 
 	void ScrollCallback(double YOffset);
 };

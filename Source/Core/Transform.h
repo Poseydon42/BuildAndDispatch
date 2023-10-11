210afd17576773d5611919ab6ccead0c3e732090
Poseydon42 <vvmposeydon@gmail.com>
2023-10-11 15:26:36 +0100

    Fixed rotation direction to clockwise in TransformationMatrix() for positive angles

--- Source/Core/Transform.h
+++ Source/Core/Transform.h
@@ -7,7 +7,7 @@ inline glm::mat4 TransformationMatrix(glm::vec2 Translation = { 0.0f, 0.0f }, fl
 {
 	auto TransformationMatrix = glm::mat4(1.0f);
 	TransformationMatrix = glm::translate(TransformationMatrix, glm::vec3(Translation, 0.0f));
-	TransformationMatrix = glm::rotate(TransformationMatrix, Rotation, glm::vec3(0.0f, 0.0f, 1.0f));
+	TransformationMatrix = glm::rotate(TransformationMatrix, Rotation, glm::vec3(0.0f, 0.0f, -1.0f));
 	TransformationMatrix = glm::scale(TransformationMatrix, glm::vec3(Scale, 1.0f));
 	return TransformationMatrix;
 }

bcc6d657985681eab3be2d8f54a90e9345625970
Poseydon42 <vvmposeydon@gmail.com>
2023-11-13 18:58:46 +0000

    Added a single content widget slot to Panel

--- Source/UI/Widgets/Panel.cpp
+++ Source/UI/Widgets/Panel.cpp
@@ -1,24 +1,72 @@
 #include "Panel.h"
 
-std::unique_ptr<Panel> Panel::Create(std::unique_ptr<Brush> Brush, glm::vec2 CustomPreferredSize)
+std::unique_ptr<Panel> Panel::Create(std::optional<std::unique_ptr<Brush>> Brush, std::optional<std::unique_ptr<Widget>> Content)
 {
-	return std::unique_ptr<Panel>(new Panel(std::move(Brush), CustomPreferredSize));
+	return std::unique_ptr<Panel>(new Panel(std::move(Brush), std::move(Content)));
 }
 
 glm::vec2 Panel::ComputePreferredSize() const
 {
-	return m_PreferredSize;
+	if (m_Content)
+		return m_Content->ComputePreferredSize();
+	return {};
+}
+
+void Panel::Layout()
+{
+	if (!m_Content)
+		return;
+
+	auto ContentPreferredSize = m_Content->ComputePreferredSize();
+
+	auto LeftMargin   = m_Content->LeftMargin()  .GetAbsoluteValue(BoundingBox().Width() );
+	auto RightMargin  = m_Content->RightMargin() .GetAbsoluteValue(BoundingBox().Width() );
+	if (LeftMargin + ContentPreferredSize.x + RightMargin > BoundingBox().Width())
+	{
+		float Scale = (BoundingBox().Width() - ContentPreferredSize.x) / (LeftMargin + RightMargin);
+		LeftMargin *= Scale;
+		RightMargin *= Scale;
+	}
+	m_Content->BoundingBox().Left() = BoundingBox().Left() + LeftMargin;
+	m_Content->BoundingBox().Right() = BoundingBox().Right() - RightMargin;
+
+	auto TopMargin = m_Content->TopMargin().GetAbsoluteValue(BoundingBox().Height());
+	auto BottomMargin = m_Content->BottomMargin().GetAbsoluteValue(BoundingBox().Height());
+	if (TopMargin + ContentPreferredSize.y + BottomMargin > BoundingBox().Height())
+	{
+		float Scale = (BoundingBox().Height() - ContentPreferredSize.y) / (TopMargin + BottomMargin);
+		TopMargin *= Scale;
+		BottomMargin *= Scale;
+	}
+	m_Content->BoundingBox().Top() = BoundingBox().Top() - TopMargin;
+	m_Content->BoundingBox().Bottom() = BoundingBox().Bottom() + BottomMargin;
+
+	m_Content->Layout();
 }
 
 void Panel::Render(RenderBuffer& Buffer) const
 {
-	Widget::Render(Buffer);
+	if (m_Brush)
+		Buffer.Rect(BoundingBox(), *m_Brush);
 
-	Buffer.Rect(BoundingBox(), *m_Brush);
+	if (m_Content)
+		m_Content->Render(Buffer);
 }
 
-Panel::Panel(std::unique_ptr<Brush> Brush, glm::vec2 CustomPreferredSize)
-	: m_Brush(std::move(Brush))
-	, m_PreferredSize(CustomPreferredSize)
+void Panel::ForEachChild(const ForEachChildCallbackType& Callback)
+{
+	if (m_Content)
+		Callback(*m_Content);
+}
+
+void Panel::ForEachChild(const ForEachChildConstCallbackType& Callback) const
+{
+	if (m_Content)
+		Callback(*m_Content);
+}
+
+Panel::Panel(std::optional<std::unique_ptr<Brush>> Brush, std::optional<std::unique_ptr<Widget>> Content)
+	: m_Brush(Brush.has_value() ? std::move(Brush.value()) : nullptr)
+	, m_Content(Content.has_value() ? std::move(Content.value()) : nullptr)
 {
 }
--- Source/UI/Widgets/Panel.h
+++ Source/UI/Widgets/Panel.h
@@ -8,16 +8,21 @@
 class Panel : public Widget
 {
 public:
-	static std::unique_ptr<Panel> Create(std::unique_ptr<Brush> Brush, glm::vec2 CustomPreferredSize = {});
+	static std::unique_ptr<Panel> Create(std::optional<std::unique_ptr<Brush>> Brush, std::optional<std::unique_ptr<Widget>> Content = std::nullopt);
 
 	virtual glm::vec2 ComputePreferredSize() const override;
 
+	virtual void Layout() override;
+
 	virtual void Render(RenderBuffer& Buffer) const override;
 
+	virtual void ForEachChild(const ForEachChildCallbackType& Callback) override;
+	virtual void ForEachChild(const ForEachChildConstCallbackType& Callback) const override;
+
 protected:
-	Panel(std::unique_ptr<Brush> Brush, glm::vec2 CustomPreferredSize);
+	Panel(std::optional<std::unique_ptr<Brush>> Brush, std::optional<std::unique_ptr<Widget>> Content);
 
 private:
 	std::unique_ptr<Brush> m_Brush;
-	glm::vec2 m_PreferredSize;
+	std::unique_ptr<Widget> m_Content;
 };

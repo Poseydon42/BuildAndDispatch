be67c6f03a8ba890ae72b492b00dd279c6f814cd
Poseydon42 <vvmposeydon@gmail.com>
2023-12-03 18:58:51 +0000

    Added different text alignment options for labels

--- Source/UI/Widgets/Label.cpp
+++ Source/UI/Widgets/Label.cpp
@@ -2,24 +2,65 @@
 
 #include "UI/TextLayout.h"
 
-std::unique_ptr<Label> Label::Create(std::string Text, uint32_t FontSize, std::shared_ptr<Font> Font)
+std::unique_ptr<Label> Label::Create(std::string Text, uint32_t FontSize, std::shared_ptr<Font> Font, TextAlignment HorizontalAlignment, TextAlignment VerticalAlignment)
 {
-	return std::unique_ptr<Label>(new Label(std::move(Text), FontSize, std::move(Font)));
+	return std::unique_ptr<Label>(new Label(std::move(Text), FontSize, std::move(Font), HorizontalAlignment, VerticalAlignment));
 }
 
 glm::vec2 Label::ComputePreferredSize() const
 {
-	auto Result = TextLayout::Measure(m_Text, m_FontSize, *m_Font);
+	auto Result = TextLayout::Measure(m_Text, m_FontSize, *m_Font) * glm::vec2(1.0f, 2.0f);
 	return Result;
 }
 
 void Label::Render(RenderBuffer& Buffer) const
 {
-	Buffer.Text(m_Text, m_FontSize, *m_Font, BoundingBox());
+	auto TextSize = TextLayout::Measure(m_Text, m_FontSize, *m_Font);
+
+	Rect2D TextRect;
+	switch (m_HorizontalTextAlignment)
+	{
+	case TextAlignment::Begin:
+		TextRect.Left() = BoundingBox().Left();
+		TextRect.Right() = TextRect.Left() + TextSize.x;
+		break;
+	case TextAlignment::Center:
+		TextRect.Left() = BoundingBox().Left() + (BoundingBox().Width() - TextSize.x) / 2.0f;
+		TextRect.Right() = BoundingBox().Right() - (BoundingBox().Width() - TextSize.x) / 2.0f;
+		break;
+	case TextAlignment::End:
+		TextRect.Left() = BoundingBox().Left() + (BoundingBox().Width() - TextSize.x);
+		TextRect.Right() = BoundingBox().Right();
+		break;
+	default:
+		BD_UNREACHABLE();
+	}
+
+	switch (m_VerticalTextAlignment)
+	{
+	case TextAlignment::Begin:
+		TextRect.Top() = BoundingBox().Top();
+		TextRect.Bottom() = BoundingBox().Top() - TextSize.y;
+		break;
+	case TextAlignment::Center:
+		TextRect.Top() = BoundingBox().Top() - (BoundingBox().Height() - TextSize.y) / 2.0f;
+		TextRect.Bottom() = BoundingBox().Bottom() + (BoundingBox().Height() - TextSize.y) / 2.0f;
+		break;
+	case TextAlignment::End:
+		TextRect.Top() = BoundingBox().Bottom() + TextSize.y;
+		TextRect.Bottom() = BoundingBox().Bottom();
+		break;
+	default:
+		BD_UNREACHABLE();
+	}
+
+	Buffer.Text(m_Text, m_FontSize, *m_Font, TextRect);
 }
 
-Label::Label(std::string Text, uint32_t FontSize, std::shared_ptr<Font> Font)
+Label::Label(std::string Text, uint32_t FontSize, std::shared_ptr<Font> Font, TextAlignment HorizontalAlignment, TextAlignment VerticalAlignment)
 	: m_Text(std::move(Text))
+	, m_HorizontalTextAlignment(HorizontalAlignment)
+	, m_VerticalTextAlignment(VerticalAlignment)
 	, m_FontSize(FontSize)
 	, m_Font(std::move(Font))
 {
--- Source/UI/Widgets/Label.h
+++ Source/UI/Widgets/Label.h
@@ -2,10 +2,17 @@
 
 #include "UI/Widget.h"
 
+enum class TextAlignment
+{
+	Begin,
+	Center,
+	End
+};
+
 class Label : public Widget
 {
 public:
-	static std::unique_ptr<Label> Create(std::string Text, uint32_t FontSize, std::shared_ptr<Font> Font);
+	static std::unique_ptr<Label> Create(std::string Text, uint32_t FontSize, std::shared_ptr<Font> Font, TextAlignment HorizontalAlignment = TextAlignment::Center, TextAlignment VerticalAlignment = TextAlignment::Center);
 
 	virtual glm::vec2 ComputePreferredSize() const override;
 
@@ -14,11 +21,20 @@ public:
 	const std::string& Text() const { return m_Text; }
 	std::string& Text() { return m_Text; }
 
+	TextAlignment& HorizontalTextAlignment() { return m_HorizontalTextAlignment; }
+	TextAlignment HorizontalTextAlignment() const { return m_HorizontalTextAlignment; }
+
+	TextAlignment& VerticalTextAlignment() { return m_VerticalTextAlignment; }
+	TextAlignment VerticalTextAlignment() const { return m_VerticalTextAlignment; }
+
 private:
 	std::string m_Text;
+
+	TextAlignment m_HorizontalTextAlignment;
+	TextAlignment m_VerticalTextAlignment;
 
 	uint32_t m_FontSize;
 	std::shared_ptr<Font> m_Font;
 
-	Label(std::string Text, uint32_t FontSize, std::shared_ptr<Font> Font);
+	Label(std::string Text, uint32_t FontSize, std::shared_ptr<Font> Font, TextAlignment HorizontalAlignment, TextAlignment VerticalAlignment);
 };
